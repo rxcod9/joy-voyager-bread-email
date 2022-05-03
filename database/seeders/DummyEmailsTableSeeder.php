408f6877c356d7d16c4352fa8ef234b7acac7425
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/DummyEmailsTableSeeder.php
+++ database/seeders/DummyEmailsTableSeeder.php
@@ -14,7 +14,7 @@ class DummyEmailsTableSeeder extends Seeder
      */
     public function run()
     {
-        $count = 100;
+        $count = 20;
         Email::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
--- database/seeders/EmailsTableSeeder.php
+++ database/seeders/EmailsTableSeeder.php
@@ -18,7 +18,7 @@ class EmailsTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         Email::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
